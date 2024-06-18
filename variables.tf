b57dab7463a86bc9df26252db4716a1d3fcf135a
cjfvnd123 <cjfvnd123@gamil.com>
2024-06-18 10:00:06 +0900

    modified:   variables.tf

--- variables.tf
+++ variables.tf
@@ -44,3 +44,9 @@ variable "width" {
   default     = "600"
   description = "Image width in pixels."
 }
+
+variable "placeholder" {
+  default     = "placekitten.com"
+  description = "Image-as-a-service URL. Some other fun ones to try are fillmurray.com, placecage.com, placebeard.it, loremflickr.com, baconmockup.com, placeimg.com, placebear.com, placeskull.com, stevensegallery.com, placedog.net"
+}
+}
